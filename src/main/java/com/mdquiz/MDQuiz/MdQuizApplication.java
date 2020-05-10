c727eec4a15fc625aeb45b04ec03acb793cde041
nbodhe <bodhe.nikhil@gmail.com>
2020-05-10 23:13:11 +0530

    Added new repo

--- src/main/java/com/mdquiz/MDQuiz/MdQuizApplication.java
+++ src/main/java/com/mdquiz/MDQuiz/MdQuizApplication.java
@@ -21,7 +21,7 @@ public class MdQuizApplication {
 	public String index() {
     return "index";
 	}
-	@RequestMapping("/home")
+	@RequestMapping("/MDQuiz/home")
 	public String home() {
     return "home";
 }
