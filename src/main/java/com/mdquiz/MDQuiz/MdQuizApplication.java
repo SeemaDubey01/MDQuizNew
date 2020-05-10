a83543584e147ecee82d74a3699e283405a18fa1
nbodhe <bodhe.nikhil@gmail.com>
2020-05-10 15:38:02 +0530

    Added new repo

--- src/main/java/com/mdquiz/MDQuiz/MdQuizApplication.java
+++ src/main/java/com/mdquiz/MDQuiz/MdQuizApplication.java
@@ -1,4 +1,4 @@
-package com.mdquiz.net;
+package com.mdquiz.MDQuiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/mdquiz/MDQuiz/ServletInitializer.java
+++ src/main/java/com/mdquiz/MDQuiz/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.mdquiz.net;
+package com.mdquiz.MDQuiz;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
